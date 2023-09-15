6b9ae3f27b46f81b929848bda7c2362fc3cc0727
Diana-Karolina <ortegadianakarolina@gmail.com>
2023-09-15 15:25:20 -0500

    subir archivo

---
+++ src/components/home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+
+const home = () => {
+
+    const [initial, setInitial] = useState ({})
+
+    useEffect(() => {
+        tododsAsegurados()
+    },[])
+
+
+
+  return (
+    <div>home</div>
+  )
+}
+
+export default home
